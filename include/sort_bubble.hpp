ba81c5335aaf1fc098ce83b1141a292f14760101
yowlings@gmail.com <yowlings@gmail.com>
2018-02-07 00:03:51 +0800

    three sort

--- include/sort_bubble.hpp
+++ include/sort_bubble.hpp
@@ -2,33 +2,106 @@
 #include <string.h>
 using namespace std;
 
-namespace sort {
+namespace algorithm {
 
 template<typename T>
-class sort_bubble
+class sort
 {
 public:
-    sort_bubble(T *arr, int num) {
-        memcpy(data,arr,num);
+    sort(T *arr, int num):compare_times(0),swap_times(0){
+//        memcpy(data,arr,num);
+        data=arr;
         size = num;
     }
-    ~sort_bubble(){
+    ~sort(){
+
+    }
+    void swap(T *arr, int i, int j){
+        T temp;
+        temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+    void print_result(){
+        cout<<"compare times: "<<compare_times<<endl;
+        cout<<"swap times: "<<swap_times<<endl;
+        cout<<"after sort the array is: "<<endl;
+        for(int i=0;i<size;i++){
+            if(i<size-1){
+                cout<<data[i]<<", ";
+            }
+            else
+            {
+                cout<<data[i]<<endl;
+            }
+        }
+
 
     }
 
-    void sort(){
-        T temp;
-        for(int i=0,i<size,i++){
-            for(int j=size-1,j>i,j--){
-                if(data[j]<data[j-1])
+    void bubble(){
+        for(int i=0;i<size;i++){
+            for(int j=size-1;j>i;j--){
+                if(data[j]<data[j-1]){
+                    compare_times++;
+                    swap(data, j-1, j);
+                    swap_times++;
+                }
+                else{
+                    compare_times++;
+                }
+
             }
         }
     }
-    void swap
-    void print_result(){}
+
+    void select(){
+        int min;
+        for(int i=0;i<size-1;i++)
+        {
+            min = i;
+            for(int j=i+1;j<size;j++)
+            {
+                if(data[j]<data[min])
+                {
+                    min=j;
+                    compare_times++;
+                }
+                else{
+                    compare_times++;
+                }
+
+            }
+            swap(data,i,min);
+            swap_times++;
+        }
+    }
+    void insert(){
+        for(int i=1;i<size;i++)
+        {
+            for(int j=i;j>0;j--)
+            {
+                if(data[j]<data[j-1])
+                {
+                    compare_times++;
+                    swap(data,j,j-1);
+                    swap_times++;
+                }
+                else{
+                    compare_times++;
+                    break;
+                }
+            }
+        }
+    }
+
+
 
 private:
     T *data;
     int size;
+    int compare_times;
+    int swap_times;
+
 };
 }
--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "sort_bubble.hpp"
 using namespace std;
-using namespace sort;
+using namespace algorithm;
 int main()
 {
-    int arr[] = {6,5,8,2,1,4,9,3,0};
+    int arr[] = {6,5,8,2,1,4,9,3,0,7};
     int num = 10;
-    sort_bubble<int> bubble(arr, num);
-    cout << "Hello World!" << endl;
+    sort<int> sort_select(arr, num);
+    sort_select.insert();
+    sort_select.print_result();
     return 0;
 }
